eac2343c559b25cc40d3bcb6da49015e2438678b
lightrao <lightrao@hotmail.com>
2024-06-27 20:55:17 +0800

    Dynamically fetch the nonce

--- client/SendETHtoWETH.js
+++ client/SendETHtoWETH.js
@@ -12,24 +12,28 @@ const privateKey = process.env.SEPOLIA_PRIVATE_KEY;
 const walletSigner = new ethers.Wallet(privateKey, providerTestnet);
 
 const exchangeETH = async () => {
-  const sendValueHuman = "0.01";
-  const gasPrice = await providerTestnet.getGasPrice();
-  const nonce = 11; // web3.eth.getTransactionCount(myAddress)
-  const txBuild = {
-    from: myAddress, // from,
-    to: "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14", // to (WETH on Sepolia Test Network),
-    value: ethers.utils.parseEther(sendValueHuman), // value,
-    nonce: nonce, // nonce,
-    gasLimit: 100000, // gas limit,
-    gasPrice: gasPrice, // gas price
-  };
+  try {
+    const sendValueHuman = "0.01";
+    const gasPrice = await providerTestnet.getGasPrice();
+    const nonce = await providerTestnet.getTransactionCount(myAddress); // Dynamically fetch the nonce
+    const txBuild = {
+      from: myAddress, // from,
+      to: "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14", // to (WETH on Sepolia Test Network),
+      value: ethers.utils.parseEther(sendValueHuman), // value,
+      nonce: nonce, // nonce,
+      gasLimit: 100000, // gas limit,
+      gasPrice: gasPrice, // gas price
+    };
 
-  // SEND Transaction
-  const txSend = await walletSigner.sendTransaction(txBuild);
+    // SEND Transaction
+    const txSend = await walletSigner.sendTransaction(txBuild);
 
-  console.log("");
-  console.log("TX SEND");
-  console.log(txSend);
+    console.log("");
+    console.log("TX SEND");
+    console.log(txSend);
+  } catch (error) {
+    console.error("Error sending transaction:", error);
+  }
 };
 
 exchangeETH();
